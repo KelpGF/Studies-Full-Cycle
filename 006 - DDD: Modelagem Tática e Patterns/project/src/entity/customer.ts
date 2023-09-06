6094981e8b91f12d3bc1cea8be5e54035c935cdd
KelpGF <kelpgf@gmail.com>
2023-09-06 18:05:15 -0300

    feat: add basica validations in customer entity in pratic projetc in DDD: Modelagem Tática e Patterns

--- 006 - DDD: Modelagem Tática e Patterns/project/src/entity/customer.ts
+++ 006 - DDD: Modelagem Tática e Patterns/project/src/entity/customer.ts
@@ -1,20 +1,33 @@
 class Customer {
   private _id: string;
   private _name: string;
-  private _address: string;
-  private _active: boolean = true;
+  private _address: string = "";
+  private _active: boolean = false;
 
-  constructor(id: string, name: string, address: string) {
+  constructor(id: string, name: string) {
     this._id = id;
     this._name = name;
-    this._address = address;
+    this.validate();
+  }
+
+  validate() {
+    if (!this._id) {
+      throw new Error("Id is required");
+    }
+    if (!this._name) {
+      throw new Error("Name is required");
+    }
   }
 
   changeName(name: string) {
     this._name = name;
+    this.validate();
   }
 
   activate() {
+    if (!this._address) {
+      throw new Error("Address is mandatory to activate a customer");
+    }
     this._active = true;
   }
 
