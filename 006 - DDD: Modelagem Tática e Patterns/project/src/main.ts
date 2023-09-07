3a9ff7d342697aaf8395269eb173afba9edd53ca
KelpGF <kelpgf@gmail.com>
2023-09-07 14:39:09 -0300

    fix(006): ensure test get sintax erros in pratic projetc

--- 006 - DDD: Modelagem Tática e Patterns/project/src/main.ts
+++ 006 - DDD: Modelagem Tática e Patterns/project/src/main.ts
@@ -8,6 +8,6 @@ const address = new Address('Rua 1', 123, 'Centro', '12345-123')
 customer.changeAddress(address)
 customer.activate()
 
-const item1 = new OrderItem('123', 'Item 1', 10)
-const item2 = new OrderItem('123', 'Item 2', 15)
+const item1 = new OrderItem('123', 'p1', 'Item 1', 10, 3)
+const item2 = new OrderItem('123', 'p2', 'Item 2', 15, 1)
 const order = new Order('1', customer.id, [item1, item2])
