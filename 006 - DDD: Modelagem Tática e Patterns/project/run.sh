b808b2cac42bdf0397e491e157a0d65546c37fe2
KelpGF <kelpgf@gmail.com>
2023-09-08 19:21:06 -0300

    chore(006): add script to run in pratic projetc

---
+++ 006 - DDD: Modelagem Tática e Patterns/project/run.sh
@@ -0,0 +1,8 @@
+docker run \
+  --rm \
+  --name node-ddd \
+  -it \
+  -w "/usr/Studies-Full-Cycle/006 - DDD: Modelagem Tática e Patterns/project" \
+  -v "/home/kelp/dev/estudos/Studies-Full-Cycle":/usr/Studies-Full-Cycle \
+  node:16 \
+  bash
