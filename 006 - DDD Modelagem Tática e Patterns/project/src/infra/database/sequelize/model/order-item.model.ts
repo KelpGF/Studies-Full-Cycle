38a4f8bd5560eec0589681345a2fbe0a13af33ef
KelpGF <kelpgf@gmail.com>
2023-09-09 12:04:17 -0300

    feat(006): add sequelize model Order, OrderItem in pratic project

---
+++ 006 - DDD Modelagem Tática e Patterns/project/src/infra/database/sequelize/model/order-item.model.ts
@@ -0,0 +1,30 @@
+import { Model, Column, PrimaryKey, Table, ForeignKey, BelongsTo } from "sequelize-typescript";
+import ProductModel from "./product.model";
+import OrderModel from "./order.model";
+
+@Table({ tableName: "order_items", timestamps: false })
+export default class OrderItemModel extends Model {
+  @PrimaryKey
+  @Column
+  declare id: number;
+
+  @ForeignKey(() => OrderModel)
+  @Column({ allowNull: false })
+  declare orderId: number;
+
+  @ForeignKey(() => ProductModel)
+  @Column({ allowNull: false })
+  declare productId: number;
+
+  @BelongsTo(() => ProductModel)
+  declare product: ProductModel;
+
+  @Column({ allowNull: false })
+  declare name: string;
+
+  @Column({ allowNull: false })
+  declare price: number;
+
+  @Column({ allowNull: false })
+  declare quantity: number;
+}
