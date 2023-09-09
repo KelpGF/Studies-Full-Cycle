05ae4f437e6c01ed78bfc474f44da9da01f742ae
KelpGF <kelpgf@gmail.com>
2023-09-09 12:03:30 -0300

    feat(006): add OrderRepositoryInterface in pratic project

---
+++ 006 - DDD Modelagem Tática e Patterns/project/src/domain/repository/order-repository.interface.ts
@@ -0,0 +1,4 @@
+import Order from "../entity/order";
+import RepositoryInterface from "./repository-interface";
+
+export default interface OrderRepositoryInterface extends RepositoryInterface<Order> {}
