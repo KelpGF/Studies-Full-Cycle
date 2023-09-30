39f4b7ee9faa2c7bd78afb6542fee0d27d119711
KelpGF <kelpgf@gmail.com>
2023-09-30 10:11:46 -0300

    feat(008): add product service and persistence interfaces in pratic project

--- 008 - Arquitetura Hexagonal/project/application/product.go
+++ 008 - Arquitetura Hexagonal/project/application/product.go
@@ -20,6 +20,26 @@ type ProductInterface interface {
 	GetPrice() float64
 }
 
+type ProductServiceInterface interface {
+	Get(id string) (ProductInterface, error)
+	Create(name string, price float64) (ProductInterface, error)
+	Enable(product ProductInterface) (ProductInterface, error)
+	Disable(product ProductInterface) (ProductInterface, error)
+}
+
+type ProductReader interface {
+	Get(id string) (ProductInterface, error)
+}
+
+type ProductWriter interface {
+	Save(product ProductInterface) (ProductInterface, error)
+}
+
+type ProductPersistenceInterface interface {
+	ProductReader
+	ProductWriter
+}
+
 const (
 	DISABLED = "disabled"
 	ENABLED  = "enabled"
