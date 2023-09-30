746b5ee52d6ca11ab5705d8ab5fa7ec8291ef7d6
KelpGF <kelpgf@gmail.com>
2023-09-30 16:44:40 -0300

    feat(008): add route to product enable/disable in pratic project

--- 008 - Arquitetura Hexagonal/project/adapters/web/handler/product.go
+++ 008 - Arquitetura Hexagonal/project/adapters/web/handler/product.go
@@ -23,6 +23,14 @@ func MakeProductHandlers(
 	r.Handle("/product", n.With(
 		negroni.Wrap(createProduct(service)),
 	)).Methods("POST", "OPTIONS")
+
+	r.Handle("/product/{id}/enable", n.With(
+		negroni.Wrap(enableProduct(service)),
+	)).Methods("PUT", "OPTIONS")
+
+	r.Handle("/product/{id}/disable", n.With(
+		negroni.Wrap(disableProduct(service)),
+	)).Methods("PUT", "OPTIONS")
 }
 
 func getProduct(service application.ProductServiceInterface) http.Handler {
@@ -71,5 +79,63 @@ func createProduct(service application.ProductServiceInterface) http.Handler {
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
+	})
+}
+
+func enableProduct(service application.ProductServiceInterface) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		vars := mux.Vars(r)
+		id := vars["id"]
+
+		product, err := service.Get(id)
+
+		if err != nil {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		product, err = service.Enable(product)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write(jsonError(err.Error()))
+			return
+		}
+
+		err = json.NewEncoder(w).Encode(product)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+	})
+}
+
+func disableProduct(service application.ProductServiceInterface) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		vars := mux.Vars(r)
+		id := vars["id"]
+
+		product, err := service.Get(id)
+
+		if err != nil {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		product, err = service.Disable(product)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write(jsonError(err.Error()))
+			return
+		}
+
+		err = json.NewEncoder(w).Encode(product)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
 	})
 }
