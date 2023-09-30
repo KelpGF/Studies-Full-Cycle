eae1f018afa1fdd8fe73df122c2e432ec97f70e9
KelpGF <kelpgf@gmail.com>
2023-09-30 16:39:30 -0300

    feat(008): add route to product create in pratic project

--- 008 - Arquitetura Hexagonal/project/adapters/web/handler/product.go
+++ 008 - Arquitetura Hexagonal/project/adapters/web/handler/product.go
@@ -8,6 +8,7 @@ import (
 	"github.com/codegangsta/negroni"
 
 	"go-hexagonal/application"
+	"go-hexagonal/adapters/dto"
 )
 
 func MakeProductHandlers(
@@ -18,6 +19,10 @@ func MakeProductHandlers(
 	r.Handle("/product/{id}", n.With(
 		negroni.Wrap(getProduct(service)),
 	)).Methods("GET", "OPTIONS")
+
+	r.Handle("/product", n.With(
+		negroni.Wrap(createProduct(service)),
+	)).Methods("POST", "OPTIONS")
 }
 
 func getProduct(service application.ProductServiceInterface) http.Handler {
@@ -39,5 +44,32 @@ func getProduct(service application.ProductServiceInterface) http.Handler {
 			return
 		}
 
+	})
+}
+
+func createProduct(service application.ProductServiceInterface) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+
+		var productDto dto.Product
+		err := json.NewDecoder(r.Body).Decode(&productDto)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write(jsonError(err.Error()))
+			return
+		}
+
+		product, err := service.Create(productDto.Name, productDto.Price)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write(jsonError(err.Error()))
+			return
+		}
+
+		err = json.NewEncoder(w).Encode(product)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
 	})
 }
