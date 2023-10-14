1416deb2378f04aebe74fbbd04acb96c79dc7f3e
KelpGF <kelpgf@gmail.com>
2023-10-14 14:41:32 -0300

    test(009): add customer e2e test

---
+++ 009 - Clean Architecture/project/src/infra/api/__tests__/customer.e2e.spec.ts
@@ -0,0 +1,34 @@
+import { app, sequelize } from '../express'
+import request from 'supertest'
+
+describe('Customer E2E', () => {
+  beforeEach(async () => {
+    await sequelize.sync({ force: true });
+  })
+
+  afterAll(async () => {
+    await sequelize.close();
+  })
+
+  it('should create a customer', async () => {
+    const response = await request(app)
+      .post('/customers')
+      .send({
+        name: 'Kelps Gomes',
+        address: {
+          street: 'Rua 1',
+          number: 123,
+          city: 'Cidade 1',
+          zipCode: '12345678'
+        }
+      })
+
+    expect(response.status).toBe(201);
+    expect(response.body.id).toBeTruthy();
+    expect(response.body.name).toBe('Kelps Gomes');
+    expect(response.body.address.street).toBe('Rua 1');
+    expect(response.body.address.number).toBe(123);
+    expect(response.body.address.city).toBe('Cidade 1');
+    expect(response.body.address.zipCode).toBe('12345678');
+  })
+});
