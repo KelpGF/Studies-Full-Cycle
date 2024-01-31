73c29303eb68ff3a4158290fc46eea2523bd44fa
KelpGF <kelpgf@gmail.com>
2024-01-31 08:14:22 -0300

    feat(015): add notes about Replay Attack

--- 015 - Autenticação e Keycloak/04 - Fluxos de Autenticação/README.md
+++ 015 - Autenticação e Keycloak/04 - Fluxos de Autenticação/README.md
@@ -11,9 +11,54 @@ Um API rest em GoLang precisa de um fluxo diferente de uma SPA em ReactJS. Utili
 
 ## Fluxos de Autenticação - Authorization Code
 
+### Características
+
 - Comum e bem aceito pela web
 - Bem versátil: pode ser utilizado em API, SPA, mobile, client
 - Seguro pois a autenticação ocorre no Keycloak
 - Single Sign On
 
 ![Fluxos de Autenticação - Authorization Code](authrozation%2520code%2520flow.png)
+
+### Segurança
+
+Quando implementamos algum tipo de Autenticação/Autorização devemos ter noção da vulnerabilidades que esse método possui.
+
+É necessário se aprofundar um pouco para ter noção dos perigos que sua aplicação pode estar correndo.
+
+### Ataques de Rede - Replay Attack
+
+Vimos que o fluxo de login é: user -> acessa o sistema -> manda para o keycloak -> informa os dados para o keycloak -> redireciona para um rota retornando junto o auth code.
+
+#### Problemática
+
+Esse último passo, temo o seguinte código:
+
+```ts
+app.get('/callback', async (req, res) => {
+  const body = {
+    client_id: 'realm-client',
+    grant_type: 'authorization_code',
+    code: req.query.code as string,
+    redirect_uri: 'http://localhost:3000/callback',
+  }
+
+  const url = 'http://host.docker.internal:8080/realms/Realm/protocol/openid-connect/token';
+
+  const result = await fetch(url, {
+    method: 'POST',
+    body: new URLSearchParams(body),
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  }).then((response) => response.json());
+
+  res.send(result);
+})
+```
+
+Esse code esta sujeito a um reply attack. Esse tipo ataque ocorre quando o atacante possui acesso sua rede e consegue capturar a chamada de callback. Da forma que foi escrito o código, o atacante pode realizar a chamada de callback novamente e gerar novos tokens, provendo acesso indevido para ele.
+
+#### Solução - Nonce
+
+Nonce significa "number used once". Essa estratégia se baseia em gerar um valor, salvá-lo quando o login for chamado e informar para o provedor identidade. Assim, os tokens serão criados com esse valor e na rota callback será feita a validação desse nonce.
--- 015 - Autenticação e Keycloak/04 - Fluxos de Autenticação/project/modules/authorization-code/index.ts
+++ 015 - Autenticação e Keycloak/04 - Fluxos de Autenticação/project/modules/authorization-code/index.ts
@@ -1,43 +1,114 @@
 import express from 'express';
+import session from 'express-session';
+import crypto from 'crypto';
+import jwt from 'jsonwebtoken';
 
 const app = express();
 
+app.use(session({ resave: true, secret: '123456', saveUninitialized: true }));
+
+// login default
 app.get('/login', (req, res) => {
-  const params = new URLSearchParams({
-    client_id: 'kama-bakka',
-    redirect_uri: 'http://localhost:3000/callback',
-    response_type: 'code',
-    scope: 'openid',
-  });
+	const params = new URLSearchParams({
+		client_id: 'kama-bakka',
+		redirect_uri: 'http://localhost:3000/callback',
+		response_type: 'code',
+		scope: 'openid',
+	});
 
-  const url = 'http://localhost:8080/realms/Kamabakka/protocol/openid-connect/auth?'+ params.toString();
+	const url = 'http://localhost:8080/realms/Kamabakka/protocol/openid-connect/auth?' + params.toString();
 
-  console.log(url);
+	console.log(url);
 
-  res.redirect(url);
-})
-
+	res.redirect(url);
+});
 app.get('/callback', async (req, res) => {
-  const body = {
-    client_id: 'kama-bakka',
-    grant_type: 'authorization_code',
-    code: req.query.code as string,
-    redirect_uri: 'http://localhost:3000/callback',
-  }
+	const body = {
+		client_id: 'kama-bakka',
+		grant_type: 'authorization_code',
+		code: req.query.code as string,
+		redirect_uri: 'http://localhost:3000/callback',
+	};
 
-  const url = 'http://host.docker.internal:8080/realms/Kamabakka/protocol/openid-connect/token';
+	const url = 'http://host.docker.internal:8080/realms/Kamabakka/protocol/openid-connect/token';
 
-  const result = await fetch(url, {
-    method: 'POST',
-    body: new URLSearchParams(body),
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-  }).then((response) => response.json());
+	const result = await fetch(url, {
+		method: 'POST',
+		body: new URLSearchParams(body),
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+		},
+	}).then((response) => response.json());
 
-  res.send(result);
-})
+	res.send(result);
+});
+
+// login with nonce
+app.get('/login-nonce', (req, res) => {
+	const nonceCode = crypto.randomBytes(16).toString('base64');
+
+	// @ts-expect-error - type mismatch
+	req.session.nonce = nonceCode;
+	req.session.save();
+
+	const params = new URLSearchParams({
+		client_id: 'kama-bakka',
+		redirect_uri: 'http://localhost:3000/callback-once',
+		response_type: 'code',
+		scope: 'openid',
+		nonce: nonceCode,
+	});
+
+	const url = 'http://localhost:8080/realms/Kamabakka/protocol/openid-connect/auth?' + params.toString();
+
+	console.log(url);
+
+	res.redirect(url);
+});
+app.get('/callback-once', async (req, res) => {
+	const body = {
+		client_id: 'kama-bakka',
+		grant_type: 'authorization_code',
+		code: req.query.code as string,
+		redirect_uri: 'http://localhost:3000/callback-once',
+	};
+
+	const url = 'http://host.docker.internal:8080/realms/Kamabakka/protocol/openid-connect/token';
+
+	const result = await fetch(url, {
+		method: 'POST',
+		body: new URLSearchParams(body),
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+		},
+	}).then((response) => response.json());
+
+	const payloadAccessToken = jwt.decode(result.access_token) as any;
+	const payloadRefreshToken = jwt.decode(result.refresh_token) as any;
+	const payloadIdToken = jwt.decode(result.id_token) as any;
+
+	// @ts-expect-error - type mismatch
+	const nonce = req.session.nonce;
+
+	if (payloadIdToken.nonce !== nonce || payloadAccessToken.nonce !== nonce || payloadRefreshToken.nonce !== nonce) {
+		res.status(401).json({ message: 'Unauthorized' });
+		return;
+	}
+
+	// @ts-expect-error - type mismatch
+	req.session.user = payloadIdToken;
+	// @ts-expect-error - type mismatch
+	req.session.access_token = result.access_token;
+	// @ts-expect-error - type mismatch
+	req.session.refresh_token = result.refresh_token;
+	// @ts-expect-error - type mismatch
+	req.session.id_token = result.id_token;
+
+	req.session.save();
+
+	res.json(result);
+});
 
 app.listen(3000, () => {
-  console.log('[Authorization Code] Server is running');
-})
+	console.log('[Authorization Code] Server is running');
+});
