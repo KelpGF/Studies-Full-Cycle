abf0d5aa9d9e2af8afdf875eaddc987e1919b5c1
KelpGF <kelpgf@gmail.com>
2024-05-11 00:29:08 -0300

    feat(observability-04): finish Prometheus Practice

--- 027 - Observabilidade/04 - Prometheus na Prática/README.md
+++ 027 - Observabilidade/04 - Prometheus na Prática/README.md
@@ -17,3 +17,31 @@ Ferramenta que coleta métricas de containers. Ele consegue analisar a utilizaç
 Ferramenta de visualização de métricas. Permite criar dashboards com diversos tipos de gráficos.
 
 Mas criar dashboards do zero é complicado. Por isso, existem dashboards prontos que podem ser importados no [Site Oficial do Grafana](https://grafana.com/grafana/dashboards/).
+
+### Configurando Alertas
+
+- Criar um Contact Point
+- Criar um Notification Police
+- Criar Alert Rule
+
+## Adicionando um Target
+
+Para adicionar um target, é necessário editar o arquivo `prometheus.yml` e adicionar o target no campo `static_configs`.
+
+```yml
+scrape_configs:
+  - job_name: 'prometheus'
+    scrape_interval: 5s
+    static_configs:
+      - targets: ['localhost:9090']
+  - job_name: cadvisor
+    scrape_interval: 5s
+    static_configs:
+    - targets:
+      - cadvisor:8080
+  - job_name: goapp
+    scrape_interval: 5s
+    static_configs:
+    - targets:
+      - app:8181
+```
--- 027 - Observabilidade/04 - Prometheus na Prática/main.go
+++ 027 - Observabilidade/04 - Prometheus na Prática/main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"math/rand"
 	"net/http"
+	"time"
 )
 
 var onlineUsers = prometheus.NewGauge(prometheus.GaugeOpts{
@@ -21,10 +22,16 @@ var httpRequestsTotal = prometheus.NewCounterVec(prometheus.CounterOpts{
 	Help: "Count of all HTTP requests",
 }, []string{})
 
+var httpDuration = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+	Name: "goapp_http_request_duration",
+	Help: "Duration in seconds of all HTTP requests",
+}, []string{"handler"})
+
 func main() {
 	r := prometheus.NewRegistry()
 	r.MustRegister(onlineUsers)
 	r.MustRegister(httpRequestsTotal)
+	r.MustRegister(httpDuration)
 
 	go func() {
 		for {
@@ -33,11 +40,29 @@ func main() {
 	}()
 
 	home := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(time.Duration(rand.Intn(5))*time.Second)
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("Hello, World!"))
 	})
 
-	http.HandleFunc("/", promhttp.InstrumentHandlerCounter(httpRequestsTotal, home))
+	contact := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(time.Duration(rand.Intn(10))*time.Second)
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Contact Page"))
+	})
+
+	d := promhttp.InstrumentHandlerDuration(
+		httpDuration.MustCurryWith(prometheus.Labels{"handler": "home"}),
+		promhttp.InstrumentHandlerCounter(httpRequestsTotal, home),
+	)
+
+	d2 := promhttp.InstrumentHandlerDuration(
+		httpDuration.MustCurryWith(prometheus.Labels{"handler": "contact"}),
+		promhttp.InstrumentHandlerCounter(httpRequestsTotal, contact),
+	)
+
+	http.HandleFunc("/", d)
+	http.HandleFunc("/contact", d2)
 	http.Handle("/metrics", promhttp.HandlerFor(r, promhttp.HandlerOpts{}))
 
 	log.Fatal(http.ListenAndServe(":8181", nil))
